98ebc494cc2efa4982c0631395fd916c22b02834
jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>
2022-08-04 15:23:32 -0300

    refactoring the GDriveService class and creating the downloadFile function
    Co-authored-by: pedr0-ops <pedro402018@outlook.com>

--- src/api/service/GDriveService.js
+++ src/api/service/GDriveService.js
@@ -5,9 +5,9 @@ const scopes = ["https://www.googleapis.com/auth/drive"];
 class GDriveService {
   authenticateCredentials(credentials) {
     const auth = new google.auth.JWT(
-      this.credentials.client_email,
+      credentials.client_email,
       null,
-      this.credentials.private_key,
+      credentials.private_key,
       scopes
     );
     return auth;
@@ -20,4 +20,31 @@ class GDriveService {
   createStream(nameStream) {
     return fs.createWriteStream(nameStream);
   }
+
+  async recordStream(stream, localFileName) {
+    return new Promise((resolve, reject) => {
+      const dest = this.createStream(localFileName);
+
+      stream
+        .on("error", (err) => {
+          reject(err);
+        })
+        .on("end", () => {
+          resolve(localFileName);
+        })
+        .pipe(dest);
+    });
+  }
+
+  async downloadFile(fileId, auth, localFileName) {
+    const drive = this.startGDrive(auth);
+
+    return drive.files
+      .get({ fileId, alt: "media" }, { responseType: "stream" })
+      .then((response) => {
+        return this.recordStream(response.data, localFileName);
+      });
+  }
 }
+
+module.exports = GDriveService;
