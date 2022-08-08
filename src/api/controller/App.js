7d7e9efa2e0e81a66f0584c4270393af1b8a7855
pedr0-ops <pedro402018@outlook.com>
2022-08-08 10:32:13 -0300

    creating function fillDataList for correction from bug
    Co-authored-by: jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>

--- src/api/controller/App.js
+++ src/api/controller/App.js
@@ -1,6 +1,8 @@
 const FileManager = require("../domain/FileManager.js");
 
 class App {
+    dataInputs = []
+
     constructor(regularExpression, id, credentials, typeFile) {
         this.regularExpression = regularExpression;
         this.id = id;
@@ -8,14 +10,15 @@ class App {
         this.typeFile = typeFile;
     }
 
-    dataInputs = [
-        this.regularExpression,
-        this.id,
-        this.credentials,
-        this.typeFile
-    ]
+    fillDataList(dataInputs) {
+        dataInputs.push(this.regularExpression);
+        dataInputs.push(this.id);
+        dataInputs.push(this.credentials);
+        dataInputs.push(this.typeFile);
+    }
 
-    requestGDriveSearch() {
+    async requestGDriveSearch() {
+        this.fillDataList(this.dataInputs);
         if (this.checkDataInput(this.dataInputs)) {
             const fileManager = new FileManager();
             return fileManager
@@ -39,3 +42,4 @@ class App {
 
 }
 
+module.exports = App;
