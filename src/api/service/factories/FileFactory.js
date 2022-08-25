8f280b60341e4da42324f2b424892e198c9060c7
pedr0-ops <pedro402018@outlook.com>
2022-08-25 10:37:54 -0300

    Entering error for unsupported extension
    Co-authored-by: jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>

--- src/api/service/factories/FileFactory.js
+++ src/api/service/factories/FileFactory.js
@@ -1,7 +1,7 @@
 const Pdf = require("../Pdf.js");
 
 class FileFactory {
- 
+
   getFileType(filePath) {
     return filePath.split(".").pop();
   }
@@ -9,6 +9,8 @@ class FileFactory {
   create(path) {
     if (this.getFileType(path) === "pdf") {
       return new Pdf(path);
+    } else {
+      throw new Error("Type file is not supported");
     }
   }
 }
