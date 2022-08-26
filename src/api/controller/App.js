3afe54398b4d9256d2b18aaa2677f3ebc5b5696f
pedr0-ops <pedro402018@outlook.com>
2022-08-26 17:40:23 -0300

    change in checkDataInput

--- src/api/controller/App.js
+++ src/api/controller/App.js
@@ -15,29 +15,24 @@ class App {
     dataInputs.push(this.typeFile);
   }
 
-    async requestGDriveSearch() {
-        this.fillDataList(this.dataInputs);
-        if (this.checkDataInput(this.dataInputs)) {
-            const fileManager = new FileManager();
-            return fileManager
-                .getSearchResult(
-                    this.dataInputs[0],
-                    this.dataInputs[1],
-                    this.dataInputs[2],
-                );
-        } else {
-            throw new Error("Failed request, bad data");
-        }
+  async requestGDriveSearch() {
+    this.fillDataList(this.dataInputs);
+    if (this.checkDataInput(this.dataInputs)) {
+      const fileManager = new FileManager();
+      return fileManager
+        .getSearchResult(
+          this.dataInputs[0],
+          this.dataInputs[1],
+          this.dataInputs[2],
+        );
+    } else {
+      throw new Error("Failed request, bad data");
     }
+  }
 
-    checkDataInput(dataInput) {
-        const auth = dataInput.map(data => {
-            if (data !== null) return true
-            else return false
-        });
-
-        return auth.reduce((input1, input2) => { return input1 & input2 });
-    }
+  checkDataInput(dataInput) {
+    return !dataInput.some(data => data === null);
+  }
 
 }
 
