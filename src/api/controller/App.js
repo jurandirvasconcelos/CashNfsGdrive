c9784bfbdd7b1ff6fae2a9782b00de5c56da5566
GiovanaRamalho <giovanasr854@gmail.com>
2022-08-26 17:01:45 -0300

    Refactoring App.js deleting input of regularExpression

--- src/api/controller/App.js
+++ src/api/controller/App.js
@@ -1,47 +1,44 @@
 const FileManager = require("../domain/FileManager.js");
 
 class App {
-    dataInputs = [];
+  dataInputs = [];
 
-    constructor(regularExpression, id, credentials, typeFile) {
-        this.regularExpression = regularExpression;
-        this.id = id;
-        this.credentials = credentials;
-        this.typeFile = typeFile;
+  constructor(id, credentials, typeFile) {
+    this.id = id;
+    this.credentials = credentials;
+    this.typeFile = typeFile;
+  }
+
+  fillDataList(dataInputs) {
+    dataInputs.push(this.id);
+    dataInputs.push(this.credentials);
+    dataInputs.push(this.typeFile);
+  }
+
+  async requestGDriveSearch() {
+    this.fillDataList(this.dataInputs);
+    if (this.checkDataInput(this.dataInputs)) {
+      const fileManager = new FileManager();
+      return fileManager.getSearchResult(
+        this.dataInputs[0],
+        this.dataInputs[1],
+        this.dataInputs[2]
+      );
+    } else {
+      throw new Error("Failed request, bad data");
     }
+  }
 
-    fillDataList(dataInputs) {
-        dataInputs.push(this.regularExpression);
-        dataInputs.push(this.id);
-        dataInputs.push(this.credentials);
-        dataInputs.push(this.typeFile);
-    }
+  checkDataInput(dataInput) {
+    const auth = dataInput.map((data) => {
+      if (data !== null) return true;
+      else return false;
+    });
 
-    async requestGDriveSearch() {
-        this.fillDataList(this.dataInputs);
-        if (this.checkDataInput(this.dataInputs)) {
-            const fileManager = new FileManager();
-            return fileManager
-                .getSearchResult(
-                    this.dataInputs[0],
-                    this.dataInputs[1],
-                    this.dataInputs[2],
-                    this.dataInputs[3],
-                );
-        } else {
-            throw new Error("Failed request, bad data");
-        }
-    }
-
-    checkDataInput(dataInput) {
-        const auth = dataInput.map(data => {
-            if (data !== null) return true
-            else return false
-        });
-
-        return auth.reduce((input1, input2) => { return input1 & input2 });
-    }
-
+    return auth.reduce((input1, input2) => {
+      return input1 & input2;
+    });
+  }
 }
 
 module.exports = App;
